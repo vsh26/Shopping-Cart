822a23be25ce3ee7721d9e4f72fc69a57e796c39
Vishal Kapgate <vishaldk26@gmail.com>
2025-03-12 18:43:07 +0530

    Fetch product list and Add data to HTML

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 let cartIcon = document.querySelector('.cart-icon');
+let closeCart = document.querySelector('.close-btn');
 let body = document.querySelector('body');
+
+let productListHTML = document.querySelector('.product-list');
+
+let productList = [];
 
 cartIcon.addEventListener('click', () => {
     body.classList.toggle('show-cart');
 });
 
-
-let closeCart = document.querySelector('.close-btn');
-
 closeCart.addEventListener('click', () => {
     body.classList.toggle('show-cart');
 });
+
+const addDataToHTML = () => {
+
+    productListHTML.innerHTML ='';
+
+    if(productList.length > 0){
+
+        productList.forEach(product => {
+
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item');
+
+            newProduct.innerHTML = `
+                <img src="${product.image}" alt="">
+                <h2>${product.name}</h2>
+                <div class="price">${product.price}</div>
+                <button class="add-to-cart">Add to Cart</button>
+            `;
+
+            productListHTML.appendChild(newProduct);
+        });
+    }
+
+}
+
+const initApp = () => {
+
+    // getting data from the json
+    fetch('products.json')
+        .then(response => response.json())
+        .then(data => {
+            productList = data;
+            // console.log(productList);
+            addDataToHTML();
+        });
+
+}
+initApp();
